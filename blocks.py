df8fcbd29b5a418d6660e8b386538c949eed5a70
Alexander Michel <alex.mic@web.de>
2020-11-06 12:32:21 +0100

    changed block color

--- blocks.py
+++ blocks.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 
+blockcolor = (37, 158, 0)
+
 
 class UpperBlock:
     def __init__(self, winsize):
@@ -11,7 +13,7 @@ class UpperBlock:
         self.scored = False
 
     def draw(self, win):
-        pygame.draw.rect(win, (255, 255, 255), (self.x, 0, 50, self.y))
+        pygame.draw.rect(win, blockcolor, (self.x, 0, 50, self.y))
 
     def move(self):
         self.x = self.x - 3
@@ -34,7 +36,7 @@ class LowerBlock:
         self.winsize = winsize
 
     def draw(self, win):
-        pygame.draw.rect(win, (255, 255, 255), (self.x, self.y, 50, self.winsize - self.y))
+        pygame.draw.rect(win, blockcolor, (self.x, self.y, 50, self.winsize - self.y))
 
     def move(self):
         self.x = self.x - 3
